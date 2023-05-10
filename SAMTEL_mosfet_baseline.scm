2851cc52298a22a89a85b661209a24d6fd94d750
Louis <luismapena15@gmail.com>
2023-05-09 22:16:56 -0400

    Minimum done

--- SAMTEL_mosfet_baseline.scm
+++ SAMTEL_mosfet_baseline.scm
@@ -32,11 +32,12 @@
 ; *		  with a region material and a name assigned. The first coordinate is on y (horizontal),
 ; * 	  the second coordinate is on x (vertical)
 ; ***********************************************************************************************
-(define kappa 2)                              			; scaling factor
-(define Lambda 1.5)			      						; voltage scaling factor
+(define kappa 1.3)                              		; scaling factor
 (define channel_start 0)   								; start of channel
 (define channel_end 0.035)    							; end of channel
-(define t_ox_height (* -1 (/ 0.005 kappa)))				; thickness of oxide
+(define t_ox_height (* -1 (* 0.005 kappa)))				; thickness of oxide
+(define start_s_ext (* -0.035 1.05))
+(define end_d_ext (+ 0.035 (* 0.035 1.05)))
 
 ;; silicon channel:
 (sdegeo:create-rectangle (position channel_start 0 0.0 )  (position channel_end 0.035 0.0 ) "Silicon" "channel" )
@@ -46,39 +47,40 @@
 (sdegeo:create-rectangle (position channel_start t_ox_height 0 )  (position channel_end -0.05 0 ) "Aluminum" "gate_electrode" )
 
 ;; Source metal electrode:
-(sdegeo:create-rectangle (position -0.07 0 0 )  (position -0.035 -0.05 0 ) "Aluminum" "source_electrode" )
+(sdegeo:create-rectangle (position -0.07 0 0 )  (position start_s_ext -0.05 0 ) "Aluminum" "source_electrode" )
 ;; silicon under source metal:
-(sdegeo:create-rectangle (position -0.07 0.035 0 )  (position -0.035 0 0 ) "Silicon" "source_n" )
+(sdegeo:create-rectangle (position -0.07 0.035 0 )  (position start_s_ext 0 0 ) "Silicon" "source_n" )
 
 ;; spacer oxide between metal gate and metal source:
-(sdegeo:create-rectangle (position channel_start 0 0 )  (position -0.035 -0.05 0 ) "SiO2" "spacerL" )
+(sdegeo:create-rectangle (position channel_start 0 0 )  (position start_s_ext -0.05 0 ) "SiO2" "spacerL" )
 ;; silicon between silicon under source metal and silicon channel, called source extension:
-(sdegeo:create-rectangle (position -0.035 0.02 0 )  (position channel_start 0 0 ) "Silicon" "source_n_ext" )
+(sdegeo:create-rectangle (position start_s_ext .035 0 )  (position channel_start 0 0 ) "Silicon" "source_n_ext" )
 ;; body region below source extension
-(sdegeo:create-rectangle (position -0.035 0.035 0 )  (position channel_start 0.02 0 ) "Silicon" "body_s_extension" )
+;(sdegeo:create-rectangle (position -0.035 0.005 0 )  (position channel_start 0 0 ) "Silicon" "source_n_ext" )
+;(sdegeo:create-rectangle (position -0.035 0.035 0 )  (position channel_start 0.005 0 ) "Silicon" "body_s_extension" )
 
 ;; Drain metal electrode:
-(sdegeo:create-rectangle (position 0.07 0 0 )  (position 0.105 -0.05 0.0 ) "Aluminum" "drain_electrode" )
+(sdegeo:create-rectangle (position end_d_ext 0 0 )  (position 0.105 -0.05 0.0 ) "Aluminum" "drain_electrode" )
 ;; silicon under drain metal:
-(sdegeo:create-rectangle (position 0.07 .035 0 )  (position 0.105 0 0 ) "Silicon" "drain_n" )
+(sdegeo:create-rectangle (position end_d_ext .035 0 )  (position 0.105 0 0 ) "Silicon" "drain_n" )
 
 ;; spacer oxide between metal gate and metal drain:
-(sdegeo:create-rectangle (position channel_end 0 0 )  (position 0.07 -0.05 0 ) "SiO2" "spacerR" )
+(sdegeo:create-rectangle (position channel_end 0 0 )  (position end_d_ext -0.05 0 ) "SiO2" "spacerR" )
 ;; silicon between silicon under drain metal and silicon channel, called drain extension:
-(sdegeo:create-rectangle (position channel_end 0.02 0 )  (position .07 0 0 ) "Silicon" "drain_n_ext" )
+(sdegeo:create-rectangle (position channel_end .035 0 )  (position end_d_ext 0 0 ) "Silicon" "drain_n_ext" )
 ;; body region below drain extension
-(sdegeo:create-rectangle (position channel_end 0.035 0 )  (position .07 0.02 0 ) "Silicon" "body_d_extension" )
+;(sdegeo:create-rectangle (position channel_end 0.005 0 )  (position .07 0 0 ) "Silicon" "drain_n_ext" )
+;(sdegeo:create-rectangle (position channel_end 0.035 0 )  (position .07 0.005 0 ) "Silicon" "body_d_extension" )
 
 ;; Body metal electrode: 
 (sdegeo:create-rectangle (position -0.07 0.6 0 )  (position 0.105 0.5 0 ) "Aluminum" "body_electrode" )
 ;; body silicon:
-(sdegeo:create-rectangle (position -.07 .2 0 )  (position .105 .035 0 ) "Silicon" "body1" )
-
-(sdegeo:create-rectangle (position -.07 .3 0 )  (position -0.02625 .2 0 ) "Silicon" "body_left_halo" )
-(sdegeo:create-rectangle (position -0.02625 .3 0 )  (position 0.06125 .2 0 ) "Silicon" "halo" )
-(sdegeo:create-rectangle (position 0.06125 .3 0 )  (position .105 .2 0 ) "Silicon" "body_right_halo" )
-
-(sdegeo:create-rectangle (position -.07 .5 0 )  (position .105 .3 0 ) "Silicon" "body2" )
+(sdegeo:create-rectangle (position -.07 .5 0 )  (position .105 .035 0 ) "Silicon" "body" )
+;(sdegeo:create-rectangle (position -.07 .07 0 )  (position .105 .035 0 ) "Silicon" "body1" )
+;(sdegeo:create-rectangle (position -.07 .3 0 )  (position -0.02625 .07 0 ) "Silicon" "body_left_halo" )
+;(sdegeo:create-rectangle (position -0.02625 .3 0 )  (position 0.06125 .07 0 ) "Silicon" "halo" )
+;(sdegeo:create-rectangle (position 0.06125 .3 0 )  (position .105 .07 0 ) "Silicon" "body_right_halo" )
+;(sdegeo:create-rectangle (position -.07 .5 0 )  (position .105 .3 0 ) "Silicon" "body2" )
 
 
 ; ***********************************************************************************************
@@ -88,31 +90,32 @@
 ; * 	  and the exact concentration.
 ; ***********************************************************************************************
 ;; channel doping:
-(sdedr:define-constant-profile "constant_channel_doping" "BoronActiveConcentration" 3e15)
+(sdedr:define-constant-profile "constant_channel_doping" "BoronActiveConcentration" 3e18)
 (sdedr:define-constant-profile-region "constant_channel_doping_placement" "constant_channel_doping" "channel")
 ;; drain extension doping:
-(sdedr:define-constant-profile "constant_drain_ext_doping" "PhosphorusActiveConcentration" 5e+16)
+(sdedr:define-constant-profile "constant_drain_ext_doping" "PhosphorusActiveConcentration" 5e+18)
 (sdedr:define-constant-profile-region "constant_drain_ext_doping_placement" "constant_drain_ext_doping" "drain_n_ext")
 ;; drain doping:
 (sdedr:define-constant-profile "constant_drain_doping" "PhosphorusActiveConcentration" 5e+19)
 (sdedr:define-constant-profile-region "constant_drain_doping_placement" "constant_drain_doping" "drain_n")
 ;; source extension doping:
-(sdedr:define-constant-profile "constant_source_ext_doping" "PhosphorusActiveConcentration" 5e+16)
+(sdedr:define-constant-profile "constant_source_ext_doping" "PhosphorusActiveConcentration" 5e+19)
 (sdedr:define-constant-profile-region "constant_source_ext_doping_placement" "constant_source_ext_doping" "source_n_ext")
 ;; source doping:
 (sdedr:define-constant-profile "constant_source_doping" "PhosphorusActiveConcentration" 5e+19)
 (sdedr:define-constant-profile-region "constant_source_doping_placement" "constant_source_doping" "source_n")
 ;; body doping:
 (sdedr:define-constant-profile "constant_body_doping" "BoronActiveConcentration" 5e+19)
-(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body1")
-(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body2")
-(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body_left_halo")
-(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body_right_halo")
-(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body_s_extension")
-(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body_d_extension")
+;(sdedr:define-constant-profile-region "constant_body1_doping_placement" "constant_body_doping" "body1")
+;(sdedr:define-constant-profile-region "constant_body2_doping_placement" "constant_body_doping" "body2")
+;(sdedr:define-constant-profile-region "constant_body_left_halo_doping_placement" "constant_body_doping" "body_left_halo")
+;(sdedr:define-constant-profile-region "constant_body_right_halo_doping_placement" "constant_body_doping" "body_right_halo")
+(sdedr:define-constant-profile-region "constant_body_doping_placement" "constant_body_doping" "body")
+(sdedr:define-constant-profile-region "constant_body_s_extension_doping_placement" "constant_body_doping" "body_s_extension")
+(sdedr:define-constant-profile-region "constant_body_d_extension_doping_placement" "constant_body_doping" "body_d_extension")
 ;; halo doping:
-(sdedr:define-constant-profile "constant_halo_doping" "BoronActiveConcentration" 5e+19)
-(sdedr:define-constant-profile-region "constant_source_doping_placement" "constant_halo_doping" "halo")
+;(sdedr:define-constant-profile "constant_halo_doping" "BoronActiveConcentration" 5e+20)
+;(sdedr:define-constant-profile-region "constant_halo_doping_placement" "constant_halo_doping" "halo")
 
 ;; define contacts
 (sdegeo:define-contact-set "Gate_contact" 4  (color:rgb 1 0 0 ) "##" )
